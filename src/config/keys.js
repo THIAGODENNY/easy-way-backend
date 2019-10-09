9335cc34d7d341be127d7b75c06ee2941fd126cf
Thiago Denny <thiagodenny@outlook.com>
2019-10-09 15:48:09 -0300

    initial serverless commit

--- src/config/keys.js
+++
@@ -1,5 +0,0 @@
-if (process.env.PROD == "true") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
-}
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const HelloController = require('./controllers/HelloControllers');
-
-const routes = express.Router();
-
-routes.get('/hello', HelloController.index);
-
-module.exports = routes;
