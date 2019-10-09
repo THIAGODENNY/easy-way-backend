e31fe14cf6b6d07786fbc5dc239f34df1ff37381
Thiago Denny <thiagodenny@outlook.com>
2019-10-09 18:29:19 -0300

    add show cid route

--- src/controllers/misc/cid/CidController.js
+++ src/controllers/misc/cid/CidController.js
@@ -3,5 +3,10 @@ const cidFile = require("./cid.json");
 module.exports = {
   async index(req, res) {
     return res.json(cidFile);
+  },
+  async show(req, res) {
+    return res.json(
+      cidFile.filter(e => e.codigo === req.params.uid.toUpperCase())
+    );
   }
 };
--- src/controllers/misc/cid/routes.js
+++ src/controllers/misc/cid/routes.js
@@ -4,5 +4,6 @@ const CidController = require("./CidController");
 const routes = express.Router();
 
 routes.get("/", CidController.index);
+routes.get("/:uid", CidController.show);
 
 module.exports = routes;
