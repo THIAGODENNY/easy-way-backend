320f923f0f7258467d19d944d6e21394ed872a1a
Thiago Denny <thiagodenny@outlook.com>
2019-10-09 16:50:21 -0300

    change to json

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", function(req, res) {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 
 module.exports.handler = serverless(app);
