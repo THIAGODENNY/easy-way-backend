ce9f4d27ac84df9e2edf631f5fd1486fba143a8b
Thiago Denny <thiagodenny@outlook.com>
2019-10-10 18:23:34 -0300

    add json zuado

--- src/controllers/HelloWorldController.test.js
+++ src/controllers/HelloWorldController.test.js
@@ -1,13 +1,13 @@
-const { index } = require('./HelloWorldController');
-const httpMocks = require('node-mocks-http');
+const { index } = require("./HelloWorldController");
+const httpMocks = require("node-mocks-http");
 
 test('The message should be: { message: "Hello World from Controller Test Evans" }', async () => {
-  const message = (
-    await index(
-      httpMocks.createRequest(), 
-      httpMocks.createResponse()
-    )
-  )._getJSONData();
+  const message = (await index(
+    httpMocks.createRequest(),
+    httpMocks.createResponse()
+  ))._getJSONData();
 
-  expect(message).toMatchObject({ message: "Hello World from Controller Test Evans with Tests" });
+  expect(message).toMatchObject({
+    message: "Hello World from Controller Test Evans with Tests"
+  });
 });
--- src/controllers/misc/cid/controllers/CidController.test.js
+++ src/controllers/misc/cid/controllers/CidController.test.js
@@ -1,29 +1,28 @@
-const { index, show } = require('./CidController');
-const httpMocks = require('node-mocks-http');
-const cidFile = require('./data/cid.json')
+const { index, show } = require("./CidController");
+const httpMocks = require("node-mocks-http");
+const cidFile = require("./data/cid.json");
 
 test(`The result should be: "{"codigo": "Z71", "nome": "Pe"...`, async () => {
-  const resultCid = (
-    await show(
-      httpMocks.createRequest({ "params": { uid: "Z71" } }), 
-      httpMocks.createResponse()
-    )
-  )._getJSONData();
-  const cid = [{
-    "codigo": "Z71", 
-    "nome": "Pessoas em Contato Com os Serviços de Saúde Para Outros Aconselhamentos e Conselho Médico, Não Classificados em Outra Parte"
-  }]
-  
+  const resultCid = (await show(
+    httpMocks.createRequest({ params: { uid: "Z71" } }),
+    httpMocks.createResponse()
+  ))._getJSONData();
+  const cid = [
+    {
+      codigo: "Z71",
+      nome:
+        "Pessoas em Contato Com os Serviços de Saúde Para Outros Aconselhamentos e Conselho Médico, Não Classificados em Outra Parte"
+    }
+  ];
+
   expect(resultCid).toMatchObject(cid);
 });
 
 test(`The result should be: "[{"codigo":"A00","nome":"C\u00f3lera"},{"c"...`, async () => {
-  const resultCidFile = (
-    await index(
-      httpMocks.createRequest(), 
-      httpMocks.createResponse()
-    )
-  )._getJSONData();
+  const resultCidFile = (await index(
+    httpMocks.createRequest(),
+    httpMocks.createResponse()
+  ))._getJSONData();
 
   expect(resultCidFile).toMatchObject(cidFile);
 });
