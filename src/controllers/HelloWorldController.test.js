c8cb0b2ffdc8f5d98b0ad080ea402c6f021f4865
Thiago Denny <thiagodenny@outlook.com>
2019-10-10 02:14:55 -0300

    remove of variables httpMocks

--- src/controllers/HelloWorldController.test.js
+++ src/controllers/HelloWorldController.test.js
@@ -1,8 +1,13 @@
 const { index } = require('./HelloWorldController');
 const httpMocks = require('node-mocks-http');
-const [ req, res ] = [ httpMocks.createRequest(), httpMocks.createResponse() ];
 
 test('The message should be: { message: "Hello World from Controller Test Evans" }', async () => {
-  const message = (await index(req, res))._getJSONData();
+  const message = (
+    await index(
+      httpMocks.createRequest(), 
+      httpMocks.createResponse()
+    )
+  )._getJSONData();
+
   expect(message).toMatchObject({ message: "Hello World from Controller Test Evans with Tests" });
 });
