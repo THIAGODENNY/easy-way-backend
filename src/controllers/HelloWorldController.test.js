77c755e609710c22a356b9f14fd7cae670132928
Thiago Denny <thiagodenny@outlook.com>
2019-10-14 21:41:40 -0300

    change evans to dario

--- src/controllers/HelloWorldController.test.js
+++ src/controllers/HelloWorldController.test.js
@@ -1,13 +1,13 @@
 const { index } = require("./HelloWorldController");
 const httpMocks = require("node-mocks-http");
 
-test('The message should be: { message: "Hello World from Controller Test Evans" }', async () => {
+test('The message should be: { message: "Hello World from Controller Test Dario with Tests" }', async () => {
   const message = (await index(
     httpMocks.createRequest(),
     httpMocks.createResponse()
   ))._getJSONData();
 
   expect(message).toMatchObject({
-    message: "Hello World from Controller Test Evans with Tests"
+    message: "Hello World from Controller Test Dario with Tests"
   });
 });
