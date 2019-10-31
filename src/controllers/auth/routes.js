d7cc3d1d3c48604c7a48c7c8fefdce887fec572a
Thiago Denny <thiagodenny@outlook.com>
2019-10-31 20:24:27 -0300

    add authentication in register user endpoint

--- src/controllers/auth/routes.js
+++ src/controllers/auth/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var routes = express.Router();
+var verifyToken = require('./auth/VerifyToken');
 
 global.__root   = __dirname + '/'; 
 
@@ -11,6 +12,8 @@ var UserController = require(__root + 'user/UserController');
 routes.use('/users', UserController);
 
 var AuthController = require(__root + 'auth/AuthController');
-routes.use('/jwt', AuthController);
+routes.use('/jwt/admin', (req, res) => verifyToken(req, res, AuthController, "admin"));
+var AuthControllerLogin = require(__root + 'auth/AuthControllerLogin');
+routes.use('/jwt', AuthControllerLogin);
 
 module.exports = routes;
