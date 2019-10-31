4ac73f9909cb0f83baa679c56c88301a09f435d0
Thiago Denny <thiagodenny@outlook.com>
2019-10-30 21:32:41 -0300

    Update routes.js
    add token verification

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,7 @@ const verifyToken = require("./controllers/auth/auth/VerifyToken");
 
 const routes = express.Router();
 
-//routes.get("/hello", (req, res) => verifyToken(req, res, HelloWorldController.index));
-routes.get("/hello", HelloWorldController.index);
+routes.get("/hello", (req, res) => verifyToken(req, res, HelloWorldController.index));
 routes.use("/misc", misc);
 routes.use("/auth", auth);
 
