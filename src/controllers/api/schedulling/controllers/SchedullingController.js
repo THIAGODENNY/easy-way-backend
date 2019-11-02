72bdd39a7942d8804f39eb131036f69c162121a6
Thiago Denny <thiagodenny@outlook.com>
2019-11-01 22:16:35 -0300

    add delete endpoint

--- src/controllers/api/schedulling/controllers/SchedullingController.js
+++ src/controllers/api/schedulling/controllers/SchedullingController.js
@@ -9,12 +9,21 @@ module.exports = {
     const { specialty, patient, medic, date } = req.body;
 
     const schedulling = await Schedulling.create({
-        specialty: specialty,
-        patient: patient,
-        medic: medic,
-        date: date
+      specialty: specialty,
+      patient: patient,
+      medic: medic,
+      date: date
     });
 
     return res.json(schedulling);
+  },
+  async destroy(req, res) {
+    const { id } = req.params;
+    console.log(req.params);
+    await Schedulling.find({ _id: id })
+      .remove()
+      .exec();
+
+    return res.json({ message: "ok" });
   }
 };
--- src/controllers/api/schedulling/routes.js
+++ src/controllers/api/schedulling/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const SchedullingController = require("./controllers/SchedullingController");
 const routes = express.Router();
 
-routes.get('/', SchedullingController.index);
-routes.post('/', SchedullingController.store);
+routes.get("/", SchedullingController.index);
+routes.post("/", SchedullingController.store);
+routes.delete("/:uid", SchedullingController.destroy);
 
 module.exports = routes;
