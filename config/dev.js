d366fd803a85f02e8e046cefde3569ad939e8d74
Thiago Denny <thiagodenny@outlook.com>
2019-11-08 16:41:44 -0300

    add phone environment variable

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,6 @@ module.exports = {
   MONGO_URI: `mongodb://localhost:27017/prontuario`,
   'secret': 'supersecret',
   SMS_ACCOUNTSID: process.env.SMS_ACCOUNTSID,
-  SMS_AUTHTOKEN: process.env.SMS_AUTHTOKEN
+  SMS_AUTHTOKEN: process.env.SMS_AUTHTOKEN, 
+  SMS_PHONE: process.env.SMS_PHONE
 };
--- src/controllers/misc/sms/controller/SmsController.js
+++ src/controllers/misc/sms/controller/SmsController.js
@@ -1,5 +1,16 @@
+const keys = require('../../../../../config/keys');
+const accountSid = keys.SMS_ACCOUNTSID;
+const authToken = keys.SMS_AUTHTOKEN;
+const from = keys.SMS_PHONE;
+const client = require('twilio')(accountSid, authToken);
+
 module.exports = {
-    async index (req, res){
+    async generateKey (req, res){
+        let key = Math.floor(Math.random() * 90000) + 10000;
+        const message = 'Your key from Vida Saudável : ' + key;
+        client.messages
+            .create({body: message, from: from, to: '+5519997835895'})
+            .then(message => console.log(message.sid, message));
         return res.send({message: 'ok'});
     }    
 }
