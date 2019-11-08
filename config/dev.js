66bc3c664898dbc749115a220e3f9838855d8f53
Thiago Denny <thiagodenny@outlook.com>
2019-11-08 16:31:13 -0300

    add environments variables

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,6 @@
 module.exports = {
   MONGO_URI: `mongodb://localhost:27017/prontuario`,
-  'secret': 'supersecret'
+  'secret': 'supersecret',
+  SMS_ACCOUNTSID: process.env.SMS_ACCOUNTSID,
+  SMS_AUTHTOKEN: process.env.SMS_AUTHTOKEN
 };
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  'secret': process.env.SECRET
+  'secret': process.env.SECRET, 
+  SMS_ACCOUNTSID: process.env.SMS_ACCOUNTSID,
+  SMS_AUTHTOKEN: process.env.SMS_AUTHTOKEN
 };
