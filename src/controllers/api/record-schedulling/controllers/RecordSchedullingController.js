00ed8f3eed0b4eb900355bac742ea9a0e864ee4f
alvesdario <alvesdario@fatec.sp.gov.br>
2019-11-18 15:28:27 -0300

    add route to get all schedules by date

--- src/controllers/api/record-schedulling/controllers/RecordSchedullingController.js
+++ src/controllers/api/record-schedulling/controllers/RecordSchedullingController.js
@@ -1,6 +1,6 @@
 const Record = require("../../record/model/Record");
 const Schedule = require("../../schedulling/model/Schedulling")
-const circularJson = require("circular-json")
+// const circularJson = require("circular-json")
 
 module.exports = {
   async showSchedullingsByRecordId(req, res) {
@@ -13,5 +13,32 @@ module.exports = {
         }
       })
     );
+  },
+  async showSchedullingsByMonthYear(req, res) {
+    const { id , month, year } = req.params;
+    if (id){
+      try{
+      const record = await Record.findOne({ _id: id });
+      return res.json(
+        await Schedule.find({
+          '_id': {
+            $in: record.schedules
+          },
+          'date': {
+            "$regex": month+"/"+year, 
+            "$options": "i"
+          }
+        })
+      );}
+      catch{return res.json({ "message": "Record not found!"})}
+    }
+    return res.json(
+      await Schedule.find({
+        'date': {
+          "$regex": month+"/"+year, 
+          "$options": "i"
+        }
+      })
+    )
   }
 };
