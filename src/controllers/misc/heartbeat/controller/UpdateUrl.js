afdec983cbb2b80dd2d919e2e4c2d473bb84292c
Thiago Denny <thiagodenny@outlook.com>
2019-11-18 20:14:19 -0200

    change from token to id user

--- src/controllers/misc/heartbeat/controller/UpdateUrl.js
+++ src/controllers/misc/heartbeat/controller/UpdateUrl.js
@@ -4,28 +4,18 @@ const User = require('../../../auth/user/User');
 
 module.exports = {
     async updateUrl(req, res) {
-        var token = req.headers['x-access-token'];
-        if (!token)
-            return res.status(403).send({ auth: false, message: 'No token provided.' });
+        const { id } = req.body;
+        if (!id)
+            return res.status(403).send({ auth: false, message: 'No id provided.' });
         const { url } = req.body;
-        await getUser(req, res, token, url, (user) => {
+        await getUser(req, res, id, url, (user) => {
             return res.status(200).send(user);
         });
     }
 }
 
-async function getUser(req, res, token, url, next) {
-    jwt.verify(token, config.secret, function (err, decoded) {
-        if (err)
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-        if (!decoded) {
-            return res.status(404).send({ auth: false, message: 'Token not Valid' });
-        }
-
-        // if everything is good, save to request for use in other routes
-        req.userId = decoded.id;
-        User.findById(req.userId, function (err, user) {
+async function getUser(req, res, id, url, next) {       
+        User.findById(id, function (err, user) {
             User.findByIdAndUpdate(user.id, { url: url }, { new: true }, function (err, user) {
                 if (err) return res.status(500).send("There was a problem updating the user.");
             });
@@ -33,5 +23,4 @@ async function getUser(req, res, token, url, next) {
             console.log(user);
             next(user);
         });
-    });
 }
