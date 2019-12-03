96d242f9103030c5731e02651df1737903407e68
Thiago Denny <thiagodenny@outlook.com>
2019-12-02 22:18:26 -0200

    add reset password endpoint

--- src/controllers/auth/auth/AuthControllerLogin.js
+++ src/controllers/auth/auth/AuthControllerLogin.js
@@ -38,6 +38,37 @@ router.post('/login', function(req, res) {
 
 });
 
+router.post('/reset', function(req, res) {
+
+  const { newpassword, renewpassword} = req.body;
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    
+    // check if the password is valid
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+
+    if (newpassword !== renewpassword) return res.status(401).send({auth: false, token: null, message: "password doesn't match"});
+
+    var hashedPassword = bcrypt.hashSync(newpassword, 8);
+
+    User.findOneAndUpdate({ email: req.body.email }, { password: hashedPassword }, function(err, user) {
+      console.log(user)
+    })
+    // if user is found and password is valid
+    // create a token
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+
+    // return the information including token as JSON
+    res.status(200).send({ auth: true, token: token });
+  });
+
+});
+
 router.post('/register', function(req, res) {
   const { profile } = req.body;
   if(profile && profile != 'patient') return res.status(500).send({ message: 'Only patient profile is allowed'})
